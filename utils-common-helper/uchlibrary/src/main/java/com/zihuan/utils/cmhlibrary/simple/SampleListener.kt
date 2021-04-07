c54abe858941b64c312a835d7dd69eabe13e69c8
zc <zihuancz@foxmail.com>
2021-04-07 18:58:37 +0800

    t

--- utils-common-helper/uchlibrary/src/main/java/com/zihuan/utils/cmhlibrary/simple/SampleListener.kt
+++ utils-common-helper/uchlibrary/src/main/java/com/zihuan/utils/cmhlibrary/simple/SampleListener.kt
@@ -3,8 +3,12 @@ package com.zihuan.utils.cmhlibrary.simple
 import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
 import android.animation.ValueAnimator
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.ViewPropertyAnimator
+import android.widget.EditText
 import androidx.viewpager.widget.ViewPager
+import com.zihuan.utils.cmhlibrary.isNotEmptyExtend
 
 open class SamplePageChangeListener : ViewPager.OnPageChangeListener {
     override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
@@ -20,13 +24,14 @@ open class SamplePageChangeListener : ViewPager.OnPageChangeListener {
 /**
  * ViewPager简洁监听
  */
-fun ViewPager.onPageScrolled(action: (Int,Float,Int) -> Unit) {
+fun ViewPager.onPageScrolled(action: (Int, Float, Int) -> Unit) {
     addOnPageChangeListener(object : SamplePageChangeListener() {
         override fun onPageScrolled(position: Int, offset: Float, offsetPixels: Int) {
-            action(position,offset,offsetPixels)
+            action(position, offset, offsetPixels)
         }
     })
 }
+
 fun ViewPager.onPageSelected(action: (position: Int) -> Unit) {
     addOnPageChangeListener(object : SamplePageChangeListener() {
         override fun onPageSelected(position: Int) {
@@ -34,6 +39,7 @@ fun ViewPager.onPageSelected(action: (position: Int) -> Unit) {
         }
     })
 }
+
 fun ViewPager.onPageScrollStateChanged(action: (Int) -> Unit) {
     addOnPageChangeListener(object : SamplePageChangeListener() {
         override fun onPageScrollStateChanged(state: Int) {
@@ -42,6 +48,38 @@ fun ViewPager.onPageScrollStateChanged(action: (Int) -> Unit) {
     })
 }
 
+/**
+ * EditText简洁监听
+ */
+open class TextWatcherSample : TextWatcher {
+    override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+    override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+    override fun afterTextChanged(s: Editable) {}
+}
+
+fun EditText.beforeTextChanged(action: (String) -> Unit) {
+    addTextChangedListener(object : TextWatcherSample() {
+        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+            action(s.toString())
+        }
+    })
+}
+
+fun EditText.onTextChanged(action: (String) -> Unit) {
+    addTextChangedListener(object : TextWatcherSample() {
+        override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+            action(s.toString())
+        }
+    })
+}
+
+fun EditText.afterTextChanged(action: (String) -> Unit) {
+    addTextChangedListener(object : TextWatcherSample() {
+        override fun afterTextChanged(s: Editable) {
+            action(s.toString())
+        }
+    })
+}
 
 fun Animator.animStart(action: (animation: Animator) -> Unit): Animator {
     addListener(object : AnimatorListenerAdapter() {
@@ -53,6 +91,10 @@ fun Animator.animStart(action: (animation: Animator) -> Unit): Animator {
     return this
 }
 
+
+/**
+ * 动画监听
+ */
 fun ValueAnimator.animUpdate(action: (ValueAnimator) -> Unit): ValueAnimator {
     addUpdateListener {
         action(it)
@@ -93,3 +135,4 @@ fun ViewPropertyAnimator.animEnd(action: (animation: Animator) -> Unit) {
         }
     })
 }
+
