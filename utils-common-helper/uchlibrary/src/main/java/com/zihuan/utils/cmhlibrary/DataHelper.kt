c57fb4866f9ab3a547f43f8c663db55cba4de354
zc <zihuancz@foxmail.com>
2021-04-16 19:01:26 +0800

    t

--- utils-common-helper/uchlibrary/src/main/java/com/zihuan/utils/cmhlibrary/DataHelper.kt
+++ utils-common-helper/uchlibrary/src/main/java/com/zihuan/utils/cmhlibrary/DataHelper.kt
@@ -220,6 +220,18 @@ val CharSequence.isChinese: Boolean
 val CharSequence.isEnglish: Boolean
     //    get() = matches(Regex("\\p{L}"))
     get() = matcher(this, "[a-zA-Z]+")
+//
+//val String.isEnglish: Boolean
+//    get() = toCharArray().let {
+//        var english = true
+//        it.forEach {
+//            if (!it.toString().matches(Regex("\\p{L}"))) {
+//                english = false
+//                return@forEach
+//            }
+//        }
+//        english
+//    }
 
 //是否为标点符号
 val CharSequence.isSymbol: Boolean
