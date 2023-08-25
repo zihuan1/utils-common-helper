dd08179372dd10ef75dc95fc72887100087139cc
zc <31381762+zihuan1@users.noreply.github.com>
2023-08-25 18:54:30 +0800

    t

--- utils-common-helper/uchlibrary/src/main/java/com/zihuan/utils/cmhlibrary/PermissionsHelper.kt
+++ utils-common-helper/uchlibrary/src/main/java/com/zihuan/utils/cmhlibrary/PermissionsHelper.kt
@@ -10,17 +10,30 @@ import com.github.dfqin.grantor.PermissionsUtil
  */
 inline fun storagePermission(crossinline action: (pass: Boolean) -> Unit) {
     val permission = if (Build.VERSION.SDK_INT >= 33) {
-        arrayOf(Manifest.permission.READ_MEDIA_AUDIO, Manifest.permission.READ_MEDIA_IMAGES, Manifest.permission.READ_MEDIA_VIDEO)
+        arrayOf(
+            Manifest.permission.READ_MEDIA_AUDIO,
+            Manifest.permission.READ_MEDIA_IMAGES,
+            Manifest.permission.READ_MEDIA_VIDEO
+        )
     } else {
-        arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        arrayOf(
+            Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
     }
     requestEasyPermission(*permission) {
         action(it)
     }
 }
 
-inline fun storage(crossinline action: () -> Unit) {
+inline fun storage(crossinline action: (Boolean) -> Unit) {
+    storagePermission { action(it) }
+}
+
+inline fun storageTrue(crossinline action: () -> Unit) {
     storagePermission { if (it) action() }
+}
+inline fun storageFalse(crossinline action: () -> Unit) {
+    storagePermission { if (!it) action() }
 }
 
 /**
@@ -30,7 +43,9 @@ inline fun cameraPermission(crossinline action: (pass: Boolean) -> Unit) {
     requestEasyPermission(Manifest.permission.CAMERA, action = action)
 }
 
-inline fun requestEasyPermission(vararg permissions: String, crossinline action: (pass: Boolean) -> Unit) {
+inline fun requestEasyPermission(
+    vararg permissions: String, crossinline action: (pass: Boolean) -> Unit
+) {
     var permission = object : PermissionListener {
         override fun permissionGranted(permission: Array<out String>) {
             action(true)
